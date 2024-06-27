4db4ec1bdcf786cd7a777beecb520d2b3de3393c
Touqeer <touqeerulhussnain@gmail.com>
2024-06-27 19:53:32 +0500

    practice the observable

--- src/app/FormComponents/data-form/data-form.component.ts
+++ src/app/FormComponents/data-form/data-form.component.ts
@@ -1,9 +1,11 @@
-import { Component, Inject, OnInit, inject } from '@angular/core';
+import { Component, } from '@angular/core';
 import { Person } from '../../Models/Person.model';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Observable, firstValueFrom } from 'rxjs';
 import { AsyncPipe, CommonModule } from '@angular/common';
+import { Observable, Subscribable, Subscription, take } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { error } from 'console';
 
 @Component({
   selector: 'app-data-form',
@@ -16,7 +18,7 @@ export class DataFormComponent {
 
 
   personForm = new FormGroup({
-    name: new FormControl<string>(''),
+    name: new FormControl<string>('', Validators.required),
     company: new FormControl<string>(''),
     phone: new FormControl<string>(""),
     email: new FormControl<string>(""),
@@ -24,7 +26,7 @@ export class DataFormComponent {
     moreDetails: new FormControl<string>(""),
   });
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private route: ActivatedRoute) {
 
   }
   public OnSubmitForm() {
@@ -49,12 +51,60 @@ export class DataFormComponent {
     console.log("testperson", testperson)
     console.log("person", person)
 
-    this.http.post("https://localhost:7070/api/Persons/AddPerson", person).subscribe({
+    this.http.post("https://localhost:7070/api/Persons/AddPerson", testperson).subscribe({
       next: (value) => {
         console.log("added")
 
       }
     });
+    let count = 0;
+    let internalSubscribe: Subscription;
+
+
+
+
+    let customeObserver = new Observable<any>((observer) => {
+
+      setInterval(() => {
+        count++;
+        observer.next(count);
+        if (count > 4) {
+          observer.error("value greater than 3");
+        }
+        if (count > 3) {
+          observer.complete();
+        }
+      }, 1000)
+    },
+
+    );
+    internalSubscribe = customeObserver.subscribe(data => {
+      console.log("internal observe", data);
+    },
+
+      error => {
+        console.log("error", error);
+      },
+      () => {
+        console.log("completed");
+      });
+
+    //the function that is called when the Observable is initially subscribed to. This function is given a Subscriber, 
+    //to which new values can be nexted,
+    // or an error method can be called to raise an error, or complete can be called to notify of a successful completion
+
+
+
+
+
+    setTimeout(() => {
+      console.log("unsubscribe observe",);
+      internalSubscribe.unsubscribe();
+    }, 5000);
+
+
+
+
   }
 
 
--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RoutingRoutingModule } from './routing-routing.module';
+import { DataFormComponent } from '../FormComponents/data-form/data-form.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ViewPersonsComponent } from '../FormComponents/view-persons/view-persons.component';
+import { BrowserModule } from '@angular/platform-browser';
 
+
+
+const routes: Routes = [
+  { path: 'form', component: DataFormComponent },
+  { path: 'view', component: ViewPersonsComponent },
+  { path: '', redirectTo: '/form', pathMatch: 'full' },
+]
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    RoutingRoutingModule
+    RoutingRoutingModule,
+    BrowserModule, RouterModule.forRoot(routes)
   ]
 })
 export class RoutingModule { }
+
